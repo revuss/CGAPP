cc4e6da0ef283f1f3223d23d32b776d4fcd4f2da
revuss <adam.evyj@gmail.com>
2024-11-19 08:59:37 +0530

    change api

--- app/services/globalServices.ts
+++ app/services/globalServices.ts
@@ -12,7 +12,7 @@ function handleError(err: unknown) {
 }
 
 function getBaseUrl() {
-  return process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api";
+  return "https://cytogenesis-beta.vercel.app/api/";
 }
 
 export async function getRequest(api: string, queryString: string = "") {
